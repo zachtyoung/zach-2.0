54966d813a2866088744f7434132a665e2c67907
Zach Young <zach.young@kochcreativegroup.com>
2024-11-11 00:54:14 -0600

    saving initial progress to source control

---
+++ src/pages/Home/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
+import Icon from "../../components/Icon"
+import { initialIcons } from "../../assets/content/initialIcons"
+import styles from "./Home.module.css"
+
+const Home = () => {
+	const [icons, setIcons] = useState(initialIcons)
+
+	const updatePosition = (id, newPosition) => {
+		setIcons((prevIcons) =>
+			prevIcons.map((icon) =>
+				icon.id === id ? { ...icon, position: newPosition } : icon
+			)
+		)
+	}
+
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<div className={styles.desktop}>
+				{icons.map((icon) => (
+					<Icon
+						key={icon.id}
+						id={icon.id}
+						url={icon.url}
+						text={icon.text}
+						position={icon.position}
+						updatePosition={updatePosition}
+						content={icon.content}
+					/>
+				))}
+			</div>
+		</DndProvider>
+	)
+}
+
+export default Home
