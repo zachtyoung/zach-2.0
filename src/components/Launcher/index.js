cb2abca083d3b3188ee9e9b13b3266e6b9ce5aee
Zach Young <ztyoung59@yahoo.com>
2024-11-11 22:32:00 -0600

    fun battery percentage

--- src/components/Launcher/index.js
+++ src/components/Launcher/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 import styles from "./Launcher.module.css";
 const Launcher = () => {
-  return <div className={styles.launcher}>Hello World</div>;
+  return (
+    <div className={styles.launcher}>
+      <div class="window">
+        <div class="title-bar">
+          <div class="title-bar-text">Internet Center</div>
+        </div>
+        <div class="window-body">
+          <ul>
+            <li>Shopping</li>
+            <li>Entertainment</li>
+            <li>Finance</li>
+            <li>Connect</li>
+            <li>Search</li>
+            <li>People</li>
+            <li>E-Mail</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Launcher;
--- src/components/StartMenu/index.js
+++ src/components/StartMenu/index.js
@@ -1,26 +1,23 @@
 // src/components/StartMenu/index.js
-import React from "react"
-import styles from "./StartMenu.module.css"
+import React from "react";
+import styles from "./StartMenu.module.css";
 
 const StartMenu = ({ isOpen, onClose }) => {
-	if (!isOpen) return null
+  if (!isOpen) return null;
 
-	return (
-		<div className={styles.startMenu}>
-			<div className={styles.menuItem}>Windows Update</div>
-			<div className={styles.menuItem}>New Office Document</div>
-			<div className={styles.menuItem}>Open Office Document</div>
-			<div className={styles.menuItem}>Programs</div>
-			<div className={styles.menuItem}>Favorites</div>
-			<div className={styles.menuItem}>Documents</div>
-			<div className={styles.menuItem}>Settings</div>
-			<div className={styles.menuItem}>Find</div>
-			<div className={styles.menuItem}>Help</div>
-			<div className={styles.menuItem}>Run...</div>
-			<div className={styles.menuItem}>Log Off...</div>
-			<div className={styles.menuItem}>Shut Down...</div>
-		</div>
-	)
-}
+  return (
+    <div className={styles.startMenu}>
+      <div className={styles.menuItem}>Programs</div>
+      <div className={styles.menuItem}>Favorites</div>
+      <div className={styles.menuItem}>Documents</div>
+      <div className={styles.menuItem}>Settings</div>
+      <div className={styles.menuItem}>Find</div>
+      <div className={styles.menuItem}>Help</div>
+      <div className={styles.menuItem}>Run...</div>
+      <div className={styles.menuItem}>Log Off...</div>
+      <div className={styles.menuItem}>Shut Down...</div>
+    </div>
+  );
+};
 
-export default StartMenu
+export default StartMenu;
