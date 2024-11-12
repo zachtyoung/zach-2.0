6e68243fc1e219aef2e0a76e1fb9a7e5e6ed5561
Zach Young <ztyoung59@yahoo.com>
2024-11-11 19:33:55 -0600

    i was tired of looking at that favicon

---
+++ src/components/Launcher/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Launcher.module.css";
+const Launcher = () => {
+  return <div className={styles.launcher}>Launcher</div>;
+};
+
+export default Launcher;
